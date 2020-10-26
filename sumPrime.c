6624e987f4c1de8fd93c3018f28d37fac2a25a6a
Adarsh Kumar <58614055+i-adarsh@users.noreply.github.com>
2020-10-26 17:14:37 +0530

    Update sumPrime.c

--- sumPrime.c
+++ sumPrime.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 
-int checkPrime(int n);
+int Prime(int n);
+
 int main() {
-    int n, i, flag = 0;
+    int n, i, count = 0;
     printf("Enter a positive integer: ");
     scanf("%d", &n);
 
     for (i = 2; i <= n / 2; ++i) {
-        // condition for i to be a prime number
-        if (checkPrime(i) == 1) {
-            // condition for n-i to be a prime number
-            if (checkPrime(n - i) == 1) {
+
+        if (isPrime(i) == 1) {
+
+            if (isPrime(n - i) == 1) {
                 printf("%d  can be Written as : %d + %d\n", n, i, n - i);
-                flag = 1;
+                count = 1;
             }
         }
     }
-    if (flag == 0)
+    if (count == 0)
         printf("%d cannot be expressed as the sum of two prime numbers.\n", n);
 
     return 0;
 }
 
-// function to check prime number
-int checkPrime(int n) {
-    int i, isPrime = 1;
+
+int isPrime(int n) {
+    int i, p = 1;
     for (i = 2; i <= n / 2; ++i) {
         if (n % i == 0) {
-            isPrime = 0;
+            p = 0;
             break;
         }
     }
-    return isPrime;
+    return p;
 }
